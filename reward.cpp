8c58034694764b5193583cd325c862a20ebd3e2c
andreew771 <chirilandreev2006@gmail.com>
2025-10-02 13:49:40 +0300

    Update reward.cpp

--- reward.cpp
+++ reward.cpp
@@ -1,5 +1,7 @@
 #include "reward.hpp"
 
-Reward::Reward() : _position{3,3} {}
-Reward::Reward(const Point& pos) : _position{pos} {}
-Point Reward::GetPosition() const { return _position; }
+struct Reward {
+    Point _position{3,3};
+
+    Point GetPosition() { return _position; }
+};
