742b939f18a36fd069b59e60d561a5ace54304b6
andreew771 <chirilandreev2006@gmail.com>
2025-10-02 13:48:32 +0300

    Update player.cpp

--- player.cpp
+++ player.cpp
@@ -1,11 +1,10 @@
 #include "player.hpp"
 
-Player::Player() : _position{0,0} {}
-Player::Player(const Point& pos) : _position{pos} {}
-Point Player::GetPosition() const { return _position; }
 void Player::Move(Direction dir) {
-    if (dir == Direction::UP) _position.y--;
-    else if (dir == Direction::DOWN) _position.y++;
-    else if (dir == Direction::LEFT) _position.x--;
-    else if (dir == Direction::RIGHT) _position.x++;
+    switch (dir) {
+        case Direction::UP:    _position.y--; break;
+        case Direction::DOWN:  _position.y++; break;
+        case Direction::LEFT:  _position.x--; break;
+        case Direction::RIGHT: _position.x++; break;
+    }
 }
