238ccae9e2fe4aa361caf036f04a2a3eeb2be518
andreew771 <chirilandreev2006@gmail.com>
2025-10-16 14:29:11 +0300

    Update map.cpp

--- map.cpp
+++ map.cpp
@@ -1,3 +1,29 @@
 #include "map.hpp"
 
-Map::Map(int w, int h) : width(w), height(h) {}
+Map::Map(int w, int h) : _width(w), _height(h) {}
+Map::Map(const Map& other) : _width(other.GetWidth()), _height(other.GetHeight()) {}
+
+int Map::GetWidth() const { return _width; }
+int Map::GetHeight() const { return _height; }
+
+Map Map::operator=(const Map& other) {
+    _width = other.GetWidth();
+    _height = other.GetHeight();
+    return *this;
+}
+
+bool Map::operator==(const Map& other) const {
+    return _width == other.GetWidth() && _height == other.GetHeight();
+}
+
+std::istream& operator>>(std::istream& in, Map& m) {
+    int w, h;
+    in >> w >> h;
+    m = Map(w, h);
+    return in;
+}
+
+std::ostream& operator<<(std::ostream& out, const Map& m) {
+    out << m.GetWidth() << " " << m.GetHeight();
+    return out;
+}
