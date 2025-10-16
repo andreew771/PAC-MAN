24767cc54eced93df7c4789176ba788141792e2d
andreew771 <chirilandreev2006@gmail.com>
2025-10-16 14:03:31 +0300

    Enhance Enemy class with copy and stream operators
    Added copy constructor, assignment operator, and stream operators for the Enemy class.

--- enemy.hpp
+++ enemy.hpp
@@ -1,12 +1,30 @@
 #pragma once
 #include "point.hpp"
 #include "direction.hpp"
+#include <iostream>
 
 class Enemy {
     Point _position;
 public:
-    Enemy();
-    Enemy(const Point& pos);
-    Point GetPosition() const;
+    Enemy() = default;
+    Enemy(const Point& pos) : _position(pos) {}
+    Enemy(const Enemy& other) : _position(other._position) {}
+
+    Enemy& operator=(const Enemy& other) {
+        if (this != &other) _position = other._position;
+        return *this;
+    }
+
+    Point GetPosition() const { return _position; }
     void Move(Direction dir);
+
+    bool operator==(const Enemy& other) const { return _position == other._position; }
+    bool operator!=(const Enemy& other) const { return !(*this == other); }
+
+    friend std::ostream& operator<<(std::ostream& os, const Enemy& e) {
+        return os << e._position;
+    }
+    friend std::istream& operator>>(std::istream& is, Enemy& e) {
+        return is >> e._position;
+    }
 };
