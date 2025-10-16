88a043dca60849758413544d5817a157e4c4a144
andreew771 <chirilandreev2006@gmail.com>
2025-10-16 14:28:16 +0300

    Refactor Reward class with constructors and operators

--- reward.cpp
+++ reward.cpp
@@ -1,7 +1,28 @@
 #include "reward.hpp"
 
-struct Reward {
-    Point _position{3,3};
+Reward::Reward() : _position(3,3) {}
+Reward::Reward(const Point& pos) : _position(pos) {}
+Reward::Reward(const Reward& other) : _position(other.GetPosition()) {}
 
-    Point GetPosition() { return _position; }
-};
+Point Reward::GetPosition() const { return _position; }
+
+Reward Reward::operator=(const Reward& other) {
+    _position = other.GetPosition();
+    return *this;
+}
+
+bool Reward::operator==(const Reward& other) const {
+    return _position == other.GetPosition();
+}
+
+std::istream& operator>>(std::istream& in, Reward& r) {
+    Point p;
+    in >> p;
+    r = Reward(p);
+    return in;
+}
+
+std::ostream& operator<<(std::ostream& out, const Reward& r) {
+    out << r.GetPosition();
+    return out;
+}
