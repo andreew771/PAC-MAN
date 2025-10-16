553e399fa8e0faaaa278a5238cdc464a08d5fc40
andreew771 <chirilandreev2006@gmail.com>
2025-10-16 14:23:54 +0300

    Update point.cpp
    Implement Point class with constructors and operator overloads

--- point.cpp
+++ point.cpp
@@ -1,10 +1,26 @@
-#include <iostream>
 #include "point.hpp"
 
-void testPoint() {
-    Point p;
-    p.x = 5;
-    p.y = 10;
+Point::Point(int _x, int _y) : x(_x), y(_y) {}
+Point::Point(const Point &other) : x(other.x), y(other.y) {}
 
-    std::cout << "Point coordinates: (" << p.x << ", " << p.y << ")\n";
+Point Point::operator=(const Point &other) {
+    x = other.x;
+    y = other.y;
+    return *this;
+}
+
+bool Point::operator==(const Point &other) const {
+    return x == other.x && y == other.y;
+}
+
+std::istream& operator>>(std::istream& in, Point& point) {
+    int x, y;
+    in >> x >> y;
+    point = Point(x, y);
+    return in;
+}
+
+std::ostream& operator<<(std::ostream& out, const Point& point) {
+    out << point.x << " " << point.y;
+    return out;
 }
