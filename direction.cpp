dbd52d776058a990b0201edaab4fa20f85116f55
andreew771 <chirilandreev2006@gmail.com>
2025-10-16 14:24:36 +0300

    Implement operator<< for Direction enum
    Added operator<< overload for Direction enum.

--- direction.cpp
+++ direction.cpp
@@ -1,10 +1,11 @@
-#include <iostream>
 #include "direction.hpp"
 
-void testDirection() {
-    Direction dir = Direction::UP;
-
-    if (dir == Direction::UP) {
-        std::cout << "UP\n";
+std::ostream& operator<<(std::ostream& out, const Direction& direction) {
+    switch (direction) {
+        case Direction::UP:    out << "UP"; break;
+        case Direction::DOWN:  out << "DOWN"; break;
+        case Direction::LEFT:  out << "LEFT"; break;
+        case Direction::RIGHT: out << "RIGHT"; break;
     }
+    return out;
 }
