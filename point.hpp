67ba4d5816ca513478cb2fa0a23fe37964c0066c
andreew771 <chirilandreev2006@gmail.com>
2025-10-16 14:01:47 +0300

    constructors and operators
    Added constructors, assignment operator, and comparison operators to the Point struct. Implemented input/output stream operators for Point.

--- point.hpp
+++ point.hpp
@@ -1,5 +1,29 @@
 #pragma once
+#include <iostream>
+
 struct Point {
-    int x;
-    int y;
+    int x{0}, y{0};
+
+    // Constructori
+    Point() = default;
+    Point(int xx, int yy) : x(xx), y(yy) {}
+    Point(const Point& other) : x(other.x), y(other.y) {}
+
+    // Operator de atribuire
+    Point& operator=(const Point& other) {
+        if (this != &other) { x = other.x; y = other.y; }
+        return *this;
+    }
+
+    // Operatori de comparare
+    bool operator==(const Point& other) const { return x == other.x && y == other.y; }
+    bool operator!=(const Point& other) const { return !(*this == other); }
+
+    // Operatori de intrare/ieșire
+    friend std::ostream& operator<<(std::ostream& os, const Point& p) {
+        return os << "(" << p.x << "," << p.y << ")";
+    }
+    friend std::istream& operator>>(std::istream& is, Point& p) {
+        return is >> p.x >> p.y;
+    }
 };
