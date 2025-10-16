023abf3cecdf7b059c6cdad14f5e0515cf5e3646
andreew771 <chirilandreev2006@gmail.com>
2025-10-16 14:26:07 +0300

    Refactor Enemy class constructor and operators

--- enemy.hpp
+++ enemy.hpp
@@ -6,25 +6,17 @@
 class Enemy {
     Point _position;
 public:
-    Enemy() = default;
-    Enemy(const Point& pos) : _position(pos) {}
-    Enemy(const Enemy& other) : _position(other._position) {}
+    Enemy();
+    Enemy(const Point& pos);
+    Enemy(const Enemy& other);
+    Point GetPosition() const;
 
-    Enemy& operator=(const Enemy& other) {
-        if (this != &other) _position = other._position;
-        return *this;
-    }
+    Enemy operator=(const Enemy& other);
+    bool operator==(const Enemy& other) const;
 
-    Point GetPosition() const { return _position; }
     void Move(Direction dir);
 
-    bool operator==(const Enemy& other) const { return _position == other._position; }
-    bool operator!=(const Enemy& other) const { return !(*this == other); }
-
-    friend std::ostream& operator<<(std::ostream& os, const Enemy& e) {
-        return os << e._position;
-    }
-    friend std::istream& operator>>(std::istream& is, Enemy& e) {
-        return is >> e._position;
-    }
+    friend std::istream& operator>>(std::istream& in, Enemy& e);
+    friend std::ostream& operator<<(std::ostream& out, const Enemy& e);
 };
+
