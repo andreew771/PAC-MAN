bc91976079a3dd9472f77996f97374555ea6b945
andreew771 <chirilandreev2006@gmail.com>
2025-10-16 14:04:51 +0300

    Enhance Player class with copy and comparison operators
    Added copy constructor, assignment operator, and comparison operators for Player class.

--- player.hpp
+++ player.hpp
@@ -1,12 +1,26 @@
 #pragma once
 #include "point.hpp"
 #include "direction.hpp"
+#include <iostream>
 
 class Player {
     Point _position;
 public:
-    Player();
-    Player(const Point& pos);
-    Point GetPosition() const;
+    Player() = default;
+    Player(const Point& pos) : _position(pos) {}
+    Player(const Player& other) : _position(other._position) {}
+
+    Player& operator=(const Player& other) {
+        if (this != &other) _position = other._position;
+        return *this;
+    }
+
+    Point GetPosition() const { return _position; }
     void Move(Direction dir);
+
+    bool operator==(const Player& other) const { return _position == other._position; }
+    bool operator!=(const Player& other) const { return !(*this == other); }
+
+    friend std::ostream& operator<<(std::ostream& os, const Player& p) { return os << p._position; }
+    friend std::istream& operator>>(std::istream& is, Player& p) { return is >> p._position; }
 };
