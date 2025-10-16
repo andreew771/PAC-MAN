bee2336865e56a9795d5649aa945bda23bf11dc1
andreew771 <chirilandreev2006@gmail.com>
2025-10-16 14:05:30 +0300

    Enhance Reward class with copy and stream operators
    Added copy constructor and assignment operator for Reward class, along with equality operators and stream operators.

--- reward.hpp
+++ reward.hpp
@@ -1,10 +1,24 @@
 #pragma once
 #include "point.hpp"
+#include <iostream>
 
 class Reward {
     Point _position;
 public:
-    Reward();
-    Reward(const Point& pos);
-    Point GetPosition() const;
+    Reward() : _position(3,3) {}
+    Reward(const Point& pos) : _position(pos) {}
+    Reward(const Reward& other) : _position(other._position) {}
+
+    Reward& operator=(const Reward& other) {
+        if (this != &other) _position = other._position;
+        return *this;
+    }
+
+    Point GetPosition() const { return _position; }
+
+    bool operator==(const Reward& other) const { return _position == other._position; }
+    bool operator!=(const Reward& other) const { return !(*this == other); }
+
+    friend std::ostream& operator<<(std::ostream& os, const Reward& r) { return os << r._position; }
+    friend std::istream& operator>>(std::istream& is, Reward& r) { return is >> r._position; }
 };
