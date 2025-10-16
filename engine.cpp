37158635f43fa8aceec24735fd37d8f37118638b
andreew771 <chirilandreev2006@gmail.com>
2025-10-16 14:29:55 +0300

    Refactor Engine constructor and add Init method

--- engine.cpp
+++ engine.cpp
@@ -1,5 +1,9 @@
 #include "engine.hpp"
 
+Engine::Engine() : _player({0,0}), _enemy({5,5}), _reward({3,3}) {}
+
+void Engine::Init() {}
+
 void Engine::Run() {
     View v;
     v.Draw(_player.GetPosition(), 'P');
