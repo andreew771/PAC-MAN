241927863da16c5ea753d64646b7a4cb161d229f
andreew771 <chirilandreev2006@gmail.com>
2025-10-29 01:42:59 +0200

    Refactor Reward class with copy and assignment operators

--- reward.hpp
+++ reward.hpp
@@ -1,10 +1,21 @@
 #pragma once
 #include "point.hpp"
+#include <iostream>
 
 class Reward {
-    Point _position;
+    Point _pos;
 public:
-    Reward();
-    Reward(const Point& pos);
-    Point GetPosition() const;
+    Reward() : _pos(3,3) {}
+    Reward(const Point& p) : _pos(p) {}
+    Reward(const Reward& other) = default;
+    Reward& operator=(const Reward& other) = default;
+
+    Point GetPosition() const { return _pos; }
+    void SetPosition(const Point& p) { _pos = p; }
+
+    bool operator==(const Reward& o) const { return _pos == o._pos; }
+    bool operator!=(const Reward& o) const { return !(*this == o); }
+
+    friend std::istream& operator>>(std::istream& in, Reward& r);
+    friend std::ostream& operator<<(std::ostream& out, const Reward& r);
 };
