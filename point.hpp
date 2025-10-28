2883ae6fd0fee5937b69f960e48d7338c2e3e2ff
andreew771 <chirilandreev2006@gmail.com>
2025-10-29 01:36:07 +0200

    Refactor Point struct with constructors and operators
    Enhanced Point struct with constructors, operators, and stream support.

--- point.hpp
+++ point.hpp
@@ -1,5 +1,14 @@
 #pragma once
+#include <iostream>
+
 struct Point {
-    int x;
-    int y;
+    int x{0}, y{0};
+    Point(int _x = 0, int _y = 0) : x(_x), y(_y) {}
+    Point(const Point& o) = default;
+    Point& operator=(const Point& o) = default;
+    bool operator==(const Point& o) const { return x==o.x && y==o.y; }
+    bool operator!=(const Point& o) const { return !(*this==o); }
 };
+
+inline std::istream& operator>>(std::istream& in, Point& p) { return in >> p.x >> p.y; }
+inline std::ostream& operator<<(std::ostream& out, const Point& p) { return out << p.x << ' ' << p.y; }
