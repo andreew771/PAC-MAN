fc3ccd27c12e98c539c2e2be5d9d3ea93363d3a6
andreew771 <chirilandreev2006@gmail.com>
2025-10-29 01:44:04 +0200

    Enhance Engine class with new features and components
    Refactor Engine class to manage multiple enemies and rewards, and include additional components like Map and View.

--- engine.hpp
+++ engine.hpp
@@ -2,13 +2,24 @@
 #include "player.hpp"
 #include "enemy.hpp"
 #include "reward.hpp"
+#include "view.hpp"
+#include "map.hpp"
+#include <vector>
+#include <memory>
 
 class Engine {
     Player _player;
-    Enemy _enemy;
-    Reward _reward;
+    std::vector<Enemy> _enemies;
+    std::vector<Reward> _rewards;
+    Map _map;
+    std::unique_ptr<View> _view;
+
 public:
     Engine();
     void Init();
     void Run();
+
+    Player& GetPlayer() { return _player; }
+    std::vector<Enemy>& GetEnemies() { return _enemies; }
+    std::vector<Reward>& GetRewards() { return _rewards; }
 };
