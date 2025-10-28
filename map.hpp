35f9cd510d4cddd151468c2a7c70d00f728e9b72
andreew771 <chirilandreev2006@gmail.com>
2025-10-29 01:43:39 +0200

    Refactor Map class to include copy and assignment operators
    Enhanced Map class with copy constructor, assignment operator, and access methods.

--- map.hpp
+++ map.hpp
@@ -1,6 +1,16 @@
 #pragma once
+#include <iostream>
+
 class Map {
-    int width, height;
+    int _width{10}, _height{10};
 public:
-    Map(int w=10, int h=10);
+    Map(int w = 10, int h = 10) : _width(w), _height(h) {}
+    Map(const Map&) = default;
+    Map& operator=(const Map&) = default;
+
+    int GetWidth() const { return _width; }
+    int GetHeight() const { return _height; }
+
+    friend std::istream& operator>>(std::istream& in, Map& m){ return in >> m._width >> m._height; }
+    friend std::ostream& operator<<(std::ostream& out, const Map& m){ return out << m._width << ' ' << m._height; }
 };
