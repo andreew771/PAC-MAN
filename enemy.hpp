b8ae3a300b66be0a4c21c4b4adbcdc43fe930f54
andreew771 <chirilandreev2006@gmail.com>
2025-10-29 01:42:06 +0200

    Enhance Enemy class with additional methods and operators
    Added copy constructor and assignment operator. Introduced SetPosition method and overloaded equality operators.

--- enemy.hpp
+++ enemy.hpp
@@ -1,12 +1,31 @@
 #pragma once
 #include "point.hpp"
 #include "direction.hpp"
+#include <iostream>
 
 class Enemy {
-    Point _position;
+    Point _pos;
 public:
-    Enemy();
-    Enemy(const Point& pos);
-    Point GetPosition() const;
-    void Move(Direction dir);
+    Enemy() = default;
+    Enemy(const Point& p) : _pos(p) {}
+    Enemy(const Enemy& other) = default;
+    Enemy& operator=(const Enemy& other) = default;
+
+    Point GetPosition() const { return _pos; }
+    void SetPosition(const Point& p) { _pos = p; }
+
+    void Move(Direction d){
+        switch(d){
+            case Direction::UP:    _pos.y--; break;
+            case Direction::DOWN:  _pos.y++; break;
+            case Direction::LEFT:  _pos.x--; break;
+            case Direction::RIGHT: _pos.x++; break;
+        }
+    }
+
+    bool operator==(const Enemy& o) const { return _pos == o._pos; }
+    bool operator!=(const Enemy& o) const { return !(*this == o); }
+
+    friend std::istream& operator>>(std::istream& in, Enemy& e);
+    friend std::ostream& operator<<(std::ostream& out, const Enemy& e);
 };
