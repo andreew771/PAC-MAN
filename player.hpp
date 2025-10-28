538248893af4773b0e4db5fbab61130236a65376
andreew771 <chirilandreev2006@gmail.com>
2025-10-29 01:41:12 +0200

    Enhance Player class with copy and assignment operators
    Added copy constructor and assignment operator. Introduced SetPosition method for updating player position.

--- player.hpp
+++ player.hpp
@@ -1,12 +1,31 @@
 #pragma once
 #include "point.hpp"
 #include "direction.hpp"
+#include <iostream>
 
 class Player {
-    Point _position;
+    Point _pos;
 public:
-    Player();
-    Player(const Point& pos);
-    Point GetPosition() const;
-    void Move(Direction dir);
+    Player() = default;
+    Player(const Point& p) : _pos(p) {}
+    Player(const Player& other) = default;
+    Player& operator=(const Player& other) = default;
+
+    Point GetPosition() const { return _pos; }
+    void SetPosition(const Point& p) { _pos = p; }
+
+    void Move(Direction d){
+        switch(d){
+            case Direction::UP:    _pos.y--; break;
+            case Direction::DOWN:  _pos.y++; break;
+            case Direction::LEFT:  _pos.x--; break;
+            case Direction::RIGHT: _pos.x++; break;
+        }
+    }
+
+    bool operator==(const Player& o) const { return _pos == o._pos; }
+    bool operator!=(const Player& o) const { return !(*this == o); }
+
+    friend std::istream& operator>>(std::istream& in, Player& p);
+    friend std::ostream& operator<<(std::ostream& out, const Player& p);
 };
