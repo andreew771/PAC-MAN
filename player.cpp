c3540ad137568a90c26db01891e805397cbe49aa
andreew771 <chirilandreev2006@gmail.com>
2025-11-12 12:49:44 +0200

    Implement Player class with constructors and operators
    Refactor Player class to include constructors, assignment operator, and score management methods.

--- player.cpp
+++ player.cpp
@@ -1,12 +1,28 @@
 #include "player.hpp"
 
+Player::Player(const std::string& n, int s) : name(n), score(s) {}
+Player::Player(const Player& other) : name(other.name), score(other.score) {}
+
+Player& Player::operator=(const Player& other) {
+    name = other.name;
+    score = other.score;
+    return *this;
+}
+
+bool Player::operator==(const Player& other) const {
+    return name == other.name && score == other.score;
+}
+
+void Player::AddScore(int value) { score += value; }
+int Player::GetScore() const { return score; }
+std::string Player::GetName() const { return name; }
+
 std::istream& operator>>(std::istream& in, Player& p) {
-    Point pt;
-    if (in >> pt) p._pos = pt;
+    in >> p.name >> p.score;
     return in;
 }
 
 std::ostream& operator<<(std::ostream& out, const Player& p) {
-    out << p._pos;
+    out << p.name << " (" << p.score << ")";
     return out;
 }
