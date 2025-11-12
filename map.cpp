61d81b954bd2ac4b8ad987d885f06da3b96817b2
andreew771 <chirilandreev2006@gmail.com>
2025-11-12 12:52:53 +0200

    Implement Generate and Display methods in Map class
    Added Generate and Display methods to Map class.

--- map.cpp
+++ map.cpp
@@ -1,3 +1,11 @@
 #include "map.hpp"
+#include <iostream>
 
 Map::Map(int w, int h) : width(w), height(h) {}
+void Map::Generate() {
+    layout.assign(height, std::string(width, '.'));
+}
+void Map::Display() const {
+    for (auto& line : layout)
+        std::cout << line << "\n";
+}
