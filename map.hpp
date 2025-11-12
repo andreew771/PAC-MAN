5a1c75b1e87752ead8858855cf73534ea1dc0f08
andreew771 <chirilandreev2006@gmail.com>
2025-11-12 12:51:55 +0200

    Update Map class with layout and new functionality
    Refactor Map class to include layout and new methods.

--- map.hpp
+++ map.hpp
@@ -1,16 +1,13 @@
 #pragma once
-#include <iostream>
+#include <vector>
+#include <string>
 
 class Map {
-    int _width{10}, _height{10};
+    int width, height;
+    std::vector<std::string> layout;
+
 public:
-    Map(int w = 10, int h = 10) : _width(w), _height(h) {}
-    Map(const Map&) = default;
-    Map& operator=(const Map&) = default;
-
-    int GetWidth() const { return _width; }
-    int GetHeight() const { return _height; }
-
-    friend std::istream& operator>>(std::istream& in, Map& m){ return in >> m._width >> m._height; }
-    friend std::ostream& operator<<(std::ostream& out, const Map& m){ return out << m._width << ' ' << m._height; }
+    Map(int w = 10, int h = 10);
+    void Generate();
+    void Display() const;
 };
