568bd573cf5267164e4494503571ad7068a30cc8
andreew771 <chirilandreev2006@gmail.com>
2025-11-12 12:50:42 +0200

    Refactor Enemy class and overload operators

--- enemy.cpp
+++ enemy.cpp
@@ -1,12 +1,22 @@
 #include "enemy.hpp"
 
-std::istream& operator>>(std::istream& in, Enemy& e) {
-    Point pt;
-    if (in >> pt) e._pos = pt;
-    return in;
+Enemy::Enemy(const std::string& t, int lvl) : type(t), dangerLevel(lvl) {}
+Enemy::Enemy(const Enemy& other) : type(other.type), dangerLevel(other.dangerLevel) {}
+
+Enemy& Enemy::operator=(const Enemy& other) {
+    type = other.type;
+    dangerLevel = other.dangerLevel;
+    return *this;
 }
 
+bool Enemy::operator==(const Enemy& other) const {
+    return type == other.type && dangerLevel == other.dangerLevel;
+}
+
+std::string Enemy::GetType() const { return type; }
+int Enemy::GetDangerLevel() const { return dangerLevel; }
+
 std::ostream& operator<<(std::ostream& out, const Enemy& e) {
-    out << e._pos;
+    out << e.type << " (lvl " << e.dangerLevel << ")";
     return out;
 }
