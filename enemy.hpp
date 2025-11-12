e6cb97fdb1db6072b64698e04149001e71aa07b0
andreew771 <chirilandreev2006@gmail.com>
2025-11-12 12:50:16 +0200

    Update Enemy class with type and danger level
    Refactor Enemy class to include type and danger level attributes.

--- enemy.hpp
+++ enemy.hpp
@@ -1,31 +1,20 @@
 #pragma once
-#include "point.hpp"
-#include "direction.hpp"
+#include <string>
 #include <iostream>
 
 class Enemy {
-    Point _pos;
+    std::string type;
+    int dangerLevel;
+
 public:
-    Enemy() = default;
-    Enemy(const Point& p) : _pos(p) {}
-    Enemy(const Enemy& other) = default;
-    Enemy& operator=(const Enemy& other) = default;
+    Enemy(const std::string& t = "Ghost", int lvl = 1);
+    Enemy(const Enemy& other);
+    Enemy& operator=(const Enemy& other);
+    bool operator==(const Enemy& other) const;
 
-    Point GetPosition() const { return _pos; }
-    void SetPosition(const Point& p) { _pos = p; }
+    std::string GetType() const;
+    int GetDangerLevel() const;
 
-    void Move(Direction d){
-        switch(d){
-            case Direction::UP:    _pos.y--; break;
-            case Direction::DOWN:  _pos.y++; break;
-            case Direction::LEFT:  _pos.x--; break;
-            case Direction::RIGHT: _pos.x++; break;
-        }
-    }
-
-    bool operator==(const Enemy& o) const { return _pos == o._pos; }
-    bool operator!=(const Enemy& o) const { return !(*this == o); }
-
-    friend std::istream& operator>>(std::istream& in, Enemy& e);
     friend std::ostream& operator<<(std::ostream& out, const Enemy& e);
 };
+
