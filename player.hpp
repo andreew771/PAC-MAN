912e789c6a0502d7d0cfcab04cc3488eb36c0f6a
andreew771 <chirilandreev2006@gmail.com>
2025-11-12 12:49:21 +0200

    Refactor Player class to add name and score
    Updated Player class to include name and score attributes with relevant methods.

--- player.hpp
+++ player.hpp
@@ -1,31 +1,23 @@
 #pragma once
-#include "point.hpp"
-#include "direction.hpp"
+#include <string>
 #include <iostream>
 
 class Player {
-    Point _pos;
+    std::string name;
+    int score;
+
 public:
-    Player() = default;
-    Player(const Point& p) : _pos(p) {}
-    Player(const Player& other) = default;
-    Player& operator=(const Player& other) = default;
+    Player(const std::string& n = "Pacman", int s = 0);
+    Player(const Player& other);
 
-    Point GetPosition() const { return _pos; }
-    void SetPosition(const Point& p) { _pos = p; }
+    Player& operator=(const Player& other);
+    bool operator==(const Player& other) const;
 
-    void Move(Direction d){
-        switch(d){
-            case Direction::UP:    _pos.y--; break;
-            case Direction::DOWN:  _pos.y++; break;
-            case Direction::LEFT:  _pos.x--; break;
-            case Direction::RIGHT: _pos.x++; break;
-        }
-    }
-
-    bool operator==(const Player& o) const { return _pos == o._pos; }
-    bool operator!=(const Player& o) const { return !(*this == o); }
+    void AddScore(int value);
+    int GetScore() const;
+    std::string GetName() const;
 
     friend std::istream& operator>>(std::istream& in, Player& p);
     friend std::ostream& operator<<(std::ostream& out, const Player& p);
 };
+
