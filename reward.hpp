1625f3c993c50a0176a866a6a56730ccd820b116
andreew771 <chirilandreev2006@gmail.com>
2025-11-12 12:51:07 +0200

    Refactor Reward class with name and value
    Updated Reward class to include name and value attributes.

--- reward.hpp
+++ reward.hpp
@@ -1,21 +1,19 @@
 #pragma once
-#include "point.hpp"
+#include <string>
 #include <iostream>
 
 class Reward {
-    Point _pos;
+    std::string name;
+    int value;
+
 public:
-    Reward() : _pos(3,3) {}
-    Reward(const Point& p) : _pos(p) {}
-    Reward(const Reward& other) = default;
-    Reward& operator=(const Reward& other) = default;
+    Reward(const std::string& n = "Cherry", int v = 10);
+    Reward(const Reward& other);
+    Reward& operator=(const Reward& other);
+    bool operator==(const Reward& other) const;
 
-    Point GetPosition() const { return _pos; }
-    void SetPosition(const Point& p) { _pos = p; }
+    int GetValue() const;
+    std::string GetName() const;
 
-    bool operator==(const Reward& o) const { return _pos == o._pos; }
-    bool operator!=(const Reward& o) const { return !(*this == o); }
-
-    friend std::istream& operator>>(std::istream& in, Reward& r);
     friend std::ostream& operator<<(std::ostream& out, const Reward& r);
 };
