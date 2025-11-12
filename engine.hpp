2663424f82707e61accfe872dc1f73c3058a2e78
andreew771 <chirilandreev2006@gmail.com>
2025-11-12 12:53:16 +0200

    Refactor Engine class to use shared_ptr for Player
    Updated Engine class to use shared_ptr for Player and removed view.

--- engine.hpp
+++ engine.hpp
@@ -1,25 +1,18 @@
 #pragma once
+#include <memory>
 #include "player.hpp"
 #include "enemy.hpp"
 #include "reward.hpp"
-#include "view.hpp"
 #include "map.hpp"
-#include <vector>
-#include <memory>
 
 class Engine {
-    Player _player;
-    std::vector<Enemy> _enemies;
-    std::vector<Reward> _rewards;
-    Map _map;
-    std::unique_ptr<View> _view;
+    std::shared_ptr<Player> player;
+    std::vector<Enemy> enemies;
+    std::vector<Reward> rewards;
+    Map map;
 
 public:
     Engine();
     void Init();
     void Run();
-
-    Player& GetPlayer() { return _player; }
-    std::vector<Enemy>& GetEnemies() { return _enemies; }
-    std::vector<Reward>& GetRewards() { return _rewards; }
 };
